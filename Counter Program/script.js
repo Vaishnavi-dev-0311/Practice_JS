9c0fb1a3c860d21bdf9b0a9a3c80dd8a09dbf635
Vaishnavi-dev-0311 <vaishnavigsakat@gmail.com>
2025-01-20 20:31:30 +0530

    Add files via upload

---
+++ Counter Program/script.js
@@ -0,0 +1,23 @@
+//Counter Program
+
+const decreasebtn = document.getElementById("decreasebtn");
+const reset = document.getElementById("reset");
+const increasebtn = document.getElementById("increasebtn");
+const countlabel = document.getElementById("countlabel");
+
+let count=0;
+
+increasebtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+decreasebtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+resetbtn.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
